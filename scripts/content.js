b695d5e1e4303da01324d6da7e42d8be08fc3cbe
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2024-03-06 10:17:13 +0000

    WIP

--- scripts/content.js
+++ scripts/content.js
@@ -2,6 +2,18 @@ document.addEventListener('keydown', (event) => {
     chrome.storage.local.get('tailwindcss_dark_mode_toggle_key').then((result) => {
         if (event.key === result?.tailwindcss_dark_mode_toggle_key) {
             document.body.classList.toggle('dark');
+
+            chrome.storage.local.set({
+                'tailwindcss_dark_mode_status' : document.body.classList.contains('dark')
+            });
+        }
+    });
+});
+
+window.addEventListener('load', () => {
+    chrome.storage.local.get('tailwindcss_dark_mode_status').then((result) => {
+        if (result?.tailwindcss_dark_mode_status ?? false) {
+            document.body.classList.add('dark');
         }
     });
 });
